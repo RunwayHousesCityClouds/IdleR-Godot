43028fb2a3e322304bc0dd71c71545315209fbb4
Sam Schock <83371366+RunwayHousesCityClouds@users.noreply.github.com>
2025-03-03 14:02:58 -0500

    factorCards now declared in class body, connected in func _ready(); func _update_supplies() now also updates factorCard quants

--- canvas_layer.gd
+++ canvas_layer.gd
@@ -15,6 +15,7 @@ var fileAsArray = file.get_file_as_string(fileName).split("\n")
 var supplies = idleR._parse_supply_data(fileAsArray)
 var factors = idleR._parse_factor_data(fileAsArray, supplies)
 var supplyCards: Array[IdleR.supplyCard] = idleR.supplyCard.toSupplyCardArray(supplies)
+var factorCards: Array[IdleR.factorCard] = idleR.factorCard.toFactorCardArray(factors)
 	
 
 func _ready():
@@ -24,15 +25,14 @@ func _ready():
 		grid_container.add_child(card)
 	
 	#Add Factor Cards
-	for f in factors:
-		var fCard = idleR.factorCard.new(f)
+	for card in factorCards:
 		#Bind buy & sell buttons
-		fCard.buyButton.pressed.connect(fCard.buy.bind(1))
-		fCard.buyButton.pressed.connect(_update_supplies.bind())
-		fCard.sellButton.pressed.connect(fCard.sell.bind(1))
-		fCard.sellButton.pressed.connect(_update_supplies.bind())
-		factor_grid_container.add_child(fCard)
-		
+		card.buyButton.pressed.connect(card.buy.bind(1))
+		card.buyButton.pressed.connect(_update_supplies.bind())
+		card.sellButton.pressed.connect(card.sell.bind(1))
+		card.sellButton.pressed.connect(_update_supplies.bind())
+		factor_grid_container.add_child(card)
+
 		#var buy10button = Button.new()
 		#buy10button.pressed.connect(fCard.buy.bind(10))
 		#buy10button.pressed.connect(_update_supplies.bind())
@@ -41,7 +41,7 @@ func _ready():
 
 func _on_button_pressed() -> void:
 	# +1 Dollar
-	supplies[0].mod_quant(1)
+	factors[0].mod_quant(1)
 	_update_supplies()
 
 func _on_button_2_pressed() -> void:
@@ -62,4 +62,6 @@ func _on_timer_timeout() -> void:
 
 func _update_supplies() -> void:
 	for card in supplyCards:
+		card.update()
+	for card in factorCards:
 		card.update()
